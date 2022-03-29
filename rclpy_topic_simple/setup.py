6e70277eda5e4347f0ab4744ce9a4492eef8df5b
goodgodgd <goodgodgd@yonsei.ac.kr>
2022-03-30 01:53:26 +0900

    rclpy topic simple package v1

--- rclpy_topic_simple/setup.py
+++ rclpy_topic_simple/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'rclpy_topic_simple'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,13 +13,23 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
+    author='ian',
+    author_email='hyukdoo.choi@sch.ac.kr',
     maintainer='ian',
-    maintainer_email='goodgodgd@yonsei.ac.kr',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='hyukdoo.choi@sch.ac.kr',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python'
+    ],
+    description='ROS 2 rclpy basic package to test topic communication',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            f'helloworld_publisher = {package_name}.helloworld_publisher:main',
+            f'helloworld_subscriber = {package_name}.helloworld_subscriber:main',
         ],
     },
 )
